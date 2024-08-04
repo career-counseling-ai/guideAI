d879a376563b09ad598a4e8d3ff72dddb0fd0f37
aryangavit <aryan.code01@gmail.com>
2024-08-05 02:22:04 +0530

    connected to mongoDB

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,16 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+dotenv.config();
+
 
 const app = express();
 
-app.listen(3000,()=>{
-    console.log('Server listening on port 3000');
-})
+//Mongoose setup
+const PORT = process.env.PORT;
+
+mongoose.connect(process.env.MONGO_URL)
+    .then(()=>{
+        app.listen(PORT,()=>console.log(`PORT:${PORT} Running`));
+    })
+    .catch((error)=>console.log(`${error}, did not connect`));
